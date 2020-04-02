d9def96e224d38ffba681da01dd6e38f127aadd6
Georg Plöckl <gp@ploeckl.de>
2020-04-02 11:26:31 +0200

    Test Commit intellij

--- src/edu/hm/ploeckl/se2/powergrid/datastore/BoardImplementation.java
+++ src/edu/hm/ploeckl/se2/powergrid/datastore/BoardImplementation.java
@@ -5,7 +5,7 @@ import edu.hm.cs.rs.powergrid.datastore.City;
 
 import java.util.List;
 import java.util.Set;
-//git test
+//git boch ein test
 public class BoardImplementation implements Board {
     private Set<City> cities;
 
